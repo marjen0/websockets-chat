48d67dc43b3aa133f23b472c87eb524c994c9cd3
Marijus <m.jenulis@gmail.com>
2019-10-27 22:08:17 +0200

    added styles

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
