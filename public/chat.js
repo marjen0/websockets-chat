9e12cec274d9ca58934fed3370981e3b78f6903e
Marijus <m.jenulis@gmail.com>
2019-10-27 23:00:35 +0200

    minor styling update

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 const socket = io.connect('http://localhost:3000');
 
 const message = document.getElementById('message'),
-      handle = document.getElementById('handle'),
+      name = document.getElementById('name'),
       btn = document.getElementById('send'),
       output = document.getElementById('output'),
       feedback = document.getElementById('feedback');
@@ -9,17 +9,17 @@ const message = document.getElementById('message'),
 btn.addEventListener('click', (e) =>{
     socket.emit('chat', {
         message: message.value,
-        handle: handle.value
+        name: name.value
     });
     message.value = "";
 });
 message.addEventListener('keypress', () => {
-    socket.emit('typing', handle.value);
+    socket.emit('typing', name.value);
 });
 
 socket.on('chat', (data) => {
     feedback.innerHTML = '';
-    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    output.innerHTML += '<p><strong>' + data.name + ': </strong>' + data.message + '</p>';
 });
 
 socket.on('typing', (data) => {
